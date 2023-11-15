01bc768166ed7dfe87d060a9ce4a95f2b152e9bc
doobu-L <seop00513@gmail.com>
2023-11-16 01:18:07 +0900

    project initialize

---
+++ LiveRanking/src/main/java/com/example/liveranking/LiveRankingApplication.java
@@ -0,0 +1,12 @@
+package com.example.liveranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveRankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiveRankingApplication.class, args);
+  }
+}
---
+++ LiveRanking/src/test/java/com/example/liveranking/LiveRankingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.liveranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveRankingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
